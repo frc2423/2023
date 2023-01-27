d7a5de5432f123006236fd476ca8bc8e6a349288
Adrian Schick <adrianmschick@gmail.com>
2023-01-26 20:28:53 -0500

    Added Java Documentation for encoder systems

--- competition/src/main/java/frc/robot/AbsoluteEncoder.java
+++ competition/src/main/java/frc/robot/AbsoluteEncoder.java
@@ -5,22 +5,36 @@ import com.revrobotics.SparkMaxAbsoluteEncoder;
 import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 
 
-
+/**
+ * This is a class repsenting an absolute encoder; An encoder that retains the zero-values of distance and angle
+ * every power cycle rather than relative to the robot's position and distance, as opposed to a relative encoder,
+ * which resets the locations of its zero-values for distance and position in relation to the robot every power cycle
+ */
 public class AbsoluteEncoder implements IEncoder {
 
     CANSparkMax motor;
     SparkMaxAbsoluteEncoder encoder;
 
+    /**
+     * Constructs a new absolute encoder for a given motor with an encoder
+     * @param motor pertains to the motor with the encoder that will be used
+     */
     public AbsoluteEncoder(CANSparkMax motor) {
         this.motor = motor;
         encoder = motor.getAbsoluteEncoder(Type.kDutyCycle);
     }
 
+    /**
+     * @see {@link com.revrobotics.SparkMaxAbsoluteEncoder#getVelocityConversionFactor()}
+     */
     @Override
     public double getVelocityConversionFactor() {
         return encoder.getVelocityConversionFactor();
     }
 
+    /**
+     * 
+     */
     @Override
     public double getVelocity() {
        return encoder.getVelocity();
@@ -51,6 +65,4 @@ public class AbsoluteEncoder implements IEncoder {
     public void setVelocityConversionFactor(double factor) {
         encoder.setVelocityConversionFactor(factor / 60);
     }
-
-   
 }
