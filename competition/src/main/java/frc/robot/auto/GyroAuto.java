b9d864bc8224118ffe988ba551d1040a46fb3de8
Skac121245 <benskraly@gmail.com>
2023-03-09 19:10:37 -0500

    finalized balance auto

--- competition/src/main/java/frc/robot/auto/GyroAuto.java
+++ competition/src/main/java/frc/robot/auto/GyroAuto.java
@@ -1,74 +1,87 @@
 package frc.robot.auto;
 
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Drivetrain;
 import frc.robot.Robot;
 import frc.robot.util.LinearScale;
 import frc.robot.util.stateMachine.State;
 import frc.robot.util.stateMachine.StateContext;
 import frc.robot.util.stateMachine.StateMachine;
 
-
-public class GyroAuto extends StateMachine{
+public class GyroAuto extends StateMachine {
     Timer timer = new Timer();
-    public GyroAuto(){
+
+    public GyroAuto() {
         super("Start");
         Robot.arm.beltStop();
     }
+
     @State(name = "Score")
-    public void taxiRun(StateContext ctx){
-    
-    Robot.arm.setShoulderSetpoint(new Rotation2d(Units.degreesToRadians(-67)));
-    Robot.arm.telescopeToSetpoint(51);
-    
-    if (ctx.getTime() > 2){
-        setState("Spit");
+    public void taxiRun(StateContext ctx) {
+
+        Robot.arm.setShoulderSetpoint(new Rotation2d(Units.degreesToRadians(-67)));
+        Robot.arm.telescopeToSetpoint(51);
+
+        if (ctx.getTime() > 2) {
+            setState("Spit");
+        }
     }
-}
 
     @State(name = "Spit")
-    public void spit(StateContext ctx){
+    public void spit(StateContext ctx) {
         Robot.arm.outtakeBelt();
         if (ctx.getTime() > 1) {
             setState("Start");
         }
     }
-    
+
     @State(name = "Start")
-    public void start(StateContext ctx){
+    public void start(StateContext ctx) {
         Robot.arm.beltStop();
         if (ctx.isInit()) {
             timer.reset();
             timer.start();
         }
-        Robot.m_drive.drive(0.75, 0, 0, true);
-        if (Robot.m_drive.m_gyro.getPitch() > 3) {
-            setState("Reached");
+        if (ctx.getTime() < 0.2) {
+            SwerveModuleState billY = new SwerveModuleState(0,
+                    Rotation2d.fromDegrees(0));
+
+            Robot.m_drive.m_frontLeft.setDesiredState(billY);
+            Robot.m_drive.m_frontRight.setDesiredState(billY);
+            Robot.m_drive.m_backLeft.setDesiredState(billY);
+            Robot.m_drive.m_backRight.setDesiredState(billY);
+
+        } else {
+            Robot.m_drive.drive(1.05, 0, 0, true);
+            if (Robot.m_drive.m_gyro.getPitch() > 3) {
+                setState("Reached");
+            }
+
         }
     }
-    
+
     @State(name = "Reached")
     public void reached(StateContext ctx) {
-        Robot.m_drive.drive(0.75, 0, 0, true);
+        Robot.m_drive.drive(1.05, 0, 0, true);
         if (Robot.m_drive.m_gyro.getPitch() < -3) {
             setState("Crossed");
         }
     }
-    
+
     @State(name = "Crossed")
     public void crossed(StateContext ctx) {
-        Robot.m_drive.drive(0.75, 0, 0, true);
+        Robot.m_drive.drive(1.05, 0, 0, true);
         if (Math.abs(Robot.m_drive.m_gyro.getPitch()) < 1.5) {
             setState("Over");
         }
     }
-    
+
     @State(name = "Over")
     public void over(StateContext ctx) {
-        Robot.m_drive.drive(-0.75, 0, 0, true);
+        Robot.m_drive.drive(-1.05, 0, 0, true);
         if (Robot.m_drive.m_gyro.getPitch() < -6) {
             setState("KeepGoing");
         }
@@ -76,63 +89,62 @@ public class GyroAuto extends StateMachine{
 
     @State(name = "KeepGoing")
     public void KeepGoing(StateContext ctx) {
-        Robot.m_drive.drive(-0.75, 0, 0, true);
-        if (ctx.getTime() > 1.5){
+        Robot.m_drive.drive(-1.05, 0, 0, true);
+        if (ctx.getTime() > 1.5) {
             setState("Balance");
         }
     }
-    
 
     double maxSpeed = 0.4;
     double minSpeed = 0;
-    double slowDownAngle = 3;
+    double slowDownAngle = 10;
     double stopAngle = 1;
-    
+
     private final LinearScale balanceScale = new LinearScale(minSpeed, maxSpeed, stopAngle, slowDownAngle);
-    
+
     @State(name = "Balance")
     public void balance(StateContext ctx) {
         // Robot.m_drive.drive(-0.4, 0, 0, true);
         var speed = balanceScale.calculate(Robot.m_drive.m_gyro.getPitch());
-        Robot.m_drive.drive(speed * 0.5, 0 , 0 , true);
+        Robot.m_drive.drive(speed * 0.5, 0, 0, true);
         if (timer.get() > 14.4) {
             setState("stop");
         }
         // if (Robot.m_drive.m_gyro.getPitch() < -3) {
-        //     Robot.m_drive.drive(-0.4, 0, 0, true);
-        //     System.out.println("1");
+        // Robot.m_drive.drive(-0.4, 0, 0, true);
+        // System.out.println("1");
         // }
         // else if(Robot.m_drive.m_gyro.getPitch() > 3) {
-        //     Robot.m_drive.drive(0.4, 0, 0, true);
-        //     System.out.println("2");
-        //     // Robot.m_drive.drive(0, 0, 0, false);
-        //     // setState("stop");
-            
+        // Robot.m_drive.drive(0.4, 0, 0, true);
+        // System.out.println("2");
+        // // Robot.m_drive.drive(0, 0, 0, false);
+        // // setState("stop");
+
         // }
         // else {
-        //     Robot.m_drive.drive(0, 0, 0, false);
-        //     System.out.println("3");
-        //     setState("stop");
-
+        // Robot.m_drive.drive(0, 0, 0, false);
+        // System.out.println("3");
+        // setState("stop");
 
         // }
     }
 
-
- @State(name = "stop")
+    @State(name = "stop")
     public void stop(StateContext ctx) {
         // Robot.m_drive.drive(0, 0, 0, false);
 
         SwerveModuleState greG = new SwerveModuleState(0,
-        Rotation2d.fromDegrees(90));
-
+                Rotation2d.fromDegrees(90));
 
         Robot.m_drive.m_frontLeft.setDesiredState(greG);
         Robot.m_drive.m_frontRight.setDesiredState(greG);
         Robot.m_drive.m_backLeft.setDesiredState(greG);
         Robot.m_drive.m_backRight.setDesiredState(greG);
 
+        if (ctx.isInit()) {
+            Robot.m_drive.setBrake(true);
+        }
+
     }
 
-    
 }
