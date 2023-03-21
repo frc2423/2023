955102b1a63e6597e5f1eca209449b4a357802e1
Amory Gaili <amorygaili@gmail.com>
2023-03-20 20:56:37 -0400

    hi! we kind of undid an hour of autoalign progress BUT names and comments are now understandable by the average person. yippee! bye!

--- competition/src/main/java/frc/robot/AutoScoreCube.java
+++ competition/src/main/java/frc/robot/AutoScoreCube.java
@@ -7,6 +7,7 @@ import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPoint;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
@@ -17,15 +18,19 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.auto.Waypoints;
+import frc.robot.util.LinearScale;
 import frc.robot.util.NtHelper;
 import frc.robot.util.stateMachine.State;
 import frc.robot.util.stateMachine.StateContext;
 import frc.robot.util.stateMachine.StateMachine;
 
 public class AutoScoreCube extends StateMachine {
-    // dw jacob we made it specific just fpr you
+    private final LinearScale robotSpin = new LinearScale(Rotation2d.fromDegrees(25).getRadians(), Math.PI,
+            Rotation2d.fromDegrees(5).getRadians(),
+            Rotation2d.fromDegrees(50).getRadians());
+    // dw jacob we made it specific just for you
     public AutoScoreCube() {
-        super("createPath"); // so rad
+        super("createPath"); 
     }
 
     public boolean scoringTag() {
@@ -40,7 +45,7 @@ public class AutoScoreCube extends StateMachine {
             if (isRed && (targetid == 1 || targetid == 2 || targetid == 3)) {
                 return true;
             } else if (!isRed && (targetid == 8 || targetid == 7 || targetid == 6)) {
-                return true; // jejwfnewon
+                return true; 
             }
         }
 
@@ -58,7 +63,7 @@ public class AutoScoreCube extends StateMachine {
             var isRed = Alliance.Red.equals(DriverStation.getAlliance());
             var targetID = bestTarget.getFiducialId();
             NtHelper.setDouble("/robot/drivetrain/APRIL_TAG_ID", targetID);
-            PathConstraints constraints = new PathConstraints(1.69, 1.69); //Nice^2
+            PathConstraints constraints = new PathConstraints(1.69, 1.69); 
             List<PathPoint> waypoints = new ArrayList<>();
 
             Pose2d start = Robot.m_drive.getPose();
@@ -70,43 +75,48 @@ public class AutoScoreCube extends StateMachine {
             Robot.trajectories.setNewTrajectoryGroup(trajectory);
             // Robot.trajectories.setNewTrajectoryGroup(Robot.m_drive.getPose(),
             //         Waypoints.aprilTagsScorePoses.get(targetID), false);
-            setState("followPath"); //sus
+            setState("followPath"); 
         }
         // staharting @ current pose ending @ score pose
     }
 
-    @State(name = "followPath") // what?!
+    @State(name = "followPath") 
     public void followPath(StateContext ctx) {
         Robot.trajectories.follow_current_path();
         if (Robot.trajectories.isFinishedWithoutTime()) {
-            setState("stahp");
+            setState("finishPath"); 
         }
-        // dew it
     }
 
-    @State(name = "stahp")
-    public void stahp(StateContext ctx) {
+    @State(name = "finishPath")
+    public void finishPath(StateContext ctx) {
         Robot.m_drive.drive(0, 0, 0, false);
-        setState("mooofawad");
-        // when stop :P
+        setState("moveForward");
     }
 
-    @State(name = "mooofawad")
-    public void mooofawad(StateContext ctx) {
+    @State(name = "moveForward")
+    public void moveForward(StateContext ctx) {
         // Robot.m_drive.drive(0, 0.2, 0, false);
-        PathConstraints constraints = new PathConstraints(1.69, 1.69); //Nice^2
+        PathConstraints constraints = new PathConstraints(1.69, 1.69);
         List<PathPoint> waypoints = new ArrayList<>();
         Pose2d start = Robot.m_drive.getPose();
-        Pose2d end = start.plus(new Transform2d(new Translation2d(Units.inchesToMeters(7.69), 0) ,new Rotation2d(0)));//noice //X = 7.69
+        Pose2d end = start.plus(new Transform2d(new Translation2d(Units.inchesToMeters(7.69), 0) ,new Rotation2d(0))); //X = 7.69
        waypoints.add(new PathPoint(start.getTranslation(), start.getRotation(), start.getRotation()));
        waypoints.add(new PathPoint(end.getTranslation(), new Rotation2d(0), end.getRotation()));
        Trajectory trajectory = PathPlanner.generatePath(constraints, false, waypoints);
         Robot.trajectories.setNewTrajectoryGroup(trajectory);
-        setState("GOO");
+        // // var angleError = MathUtil.angleModulus(0 - Robot.m_drive.getOdometryAngle().getRadians());
+        // // var rotationSpeed = robotSpin.calculate(angleError);
+        // // Robot.m_drive.drive(-0.5, 0, rotationSpeed, true);
+        // if (ctx.getTime() > 1.5) {
+        // Robot.m_drive.drive(0, 0, 0, true);
+            setState("scoreRobot");
+        // }
+
     }
 
-    @State(name = "beSWERVY")
-    public void beSWERVY(StateContext ctx) {
+    @State(name = "setSwerveStateZero")
+    public void setSwerveStateZero(StateContext ctx) {
         SwerveModuleState jilly = new SwerveModuleState(0,
                     Rotation2d.fromDegrees(0));
 
@@ -116,22 +126,21 @@ public class AutoScoreCube extends StateMachine {
             Robot.m_drive.m_backRight.setDesiredState(jilly);
     }
         
-    @State(name = "GOO")
-    public void GOO(StateContext ctx) {
+    @State(name = "followRobotPath")
+    public void followRobotPath(StateContext ctx) {
         Robot.trajectories.follow_current_path();
-        if (ctx.getTime() > 0.69) {//noice
-            setState("scahr");
+        if (ctx.getTime() > 0.69) {
+            setState("scoreRobot");
         }
     }
 
-    @State(name = "scahr")
-    public void scahr(StateContext ctx) {
+    @State(name = "scoreRobot")
+    public void scoreRobot(StateContext ctx) {
         Robot.arm.setShoulderSetpoint(new Rotation2d(Units.degreesToRadians(52)));
         Robot.arm.telescopeToSetpoint(0);
-        if (ctx.getTime() > 0.69){  //noice
+        if (ctx.getTime() > 0.69){  
         Robot.arm.outtakeBelt();
         }
-        // cahoobz
     }
 
     // public void sleep() {
