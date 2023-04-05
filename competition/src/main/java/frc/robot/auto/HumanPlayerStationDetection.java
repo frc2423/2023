198928ca158ab412d38daa52b70249ea7cc05da4
amorygalili <amorygalili@gmail.com>
2023-04-05 15:52:07 -0400

    Debugged rumble code

--- competition/src/main/java/frc/robot/auto/HumanPlayerStationDetection.java
+++ competition/src/main/java/frc/robot/auto/HumanPlayerStationDetection.java
@@ -1,30 +1,55 @@
 package frc.robot.auto;
+
 import frc.robot.Robot;
+import frc.robot.util.NtHelper;
 import frc.robot.util.PhotonRunnable;
+import frc.robot.util.PoseTransformUtils;
+import edu.wpi.first.apriltag.AprilTagFieldLayout.OriginPosition;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
 public class HumanPlayerStationDetection {
-    private static double mindistance = 10;
+    private static double maxDistance = 4; // in meters
+    private static AprilTagFieldLayout layout = null;
+
     public static boolean seesTagCloseEnough() {
-   
+        if (layout == null) {
+            try {
+                layout = AprilTagFields.k2023ChargedUp.loadAprilTagLayoutField();
+                // PV estimates will always be blue, they'll get flipped by robot thread
+                layout.setOrigin(OriginPosition.kBlueAllianceWallRightSide);
+            } catch(Exception e) {
+    
+            }
+        } 
+
         var targetId = Robot.photonEstimator.grabBestID();
-        if (targetId == null) {
+
+        if (targetId == null && layout != null) {
+            NtHelper.setDouble("/testing/distanceBlue", 0);
+            NtHelper.setDouble("/testing/distanceRed", 0);
             return false;
         }
 
         boolean isRed = Alliance.Red.equals(DriverStation.getAlliance());
         var maps = Waypoints.aprilTagsHumanPlayerStationLongName;
-        
-        var bluetag = maps.get(5);
-        var redtag = maps.get(4);
-        var pose = Robot.m_drive.getPose();
-        var distanceblue = bluetag.getTranslation().getDistance(pose.getTranslation());
-        var distancered = redtag.getTranslation().getDistance(pose.getTranslation());
 
-        if (isRed && targetId == 5 && distancered <=mindistance) {
+
+        var bluetag = layout.getTagPose(4).get().toPose2d();
+        var redtag = PoseTransformUtils.transformRedPose(layout.getTagPose(5).get().toPose2d());
+        var robotTranslation = Robot.m_drive.getPose().getTranslation();
+        var distanceBlue = bluetag.getTranslation().getDistance(robotTranslation);
+        var distanceRed = redtag.getTranslation().getDistance(robotTranslation);
+
+        NtHelper.setDouble("/testing/distanceBlue", distanceBlue);
+        NtHelper.setDouble("/testing/distanceRed", distanceRed);
+
+        if (isRed && targetId == 5 && distanceRed <= maxDistance) {
             return true;
         }
-        if (!isRed && targetId == 4 && distanceblue <=mindistance ) {
+        if (!isRed && targetId == 4 && distanceBlue <= maxDistance) {
             return true;
         }
         return false;
