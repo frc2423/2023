d78b41ac398bda44d507266fbb2744efefbe98d2
Skac121245 <benskraly@gmail.com>
2023-04-11 22:24:32 -0400

    worked on rainbow leds

--- competition/src/main/java/frc/robot/Led/KwarqsLed.java
+++ competition/src/main/java/frc/robot/Led/KwarqsLed.java
@@ -35,7 +35,7 @@ public class KwarqsLed {
         final var hue = (m_rainbowFirstPixelHue + (i * 180 / 64)) % 180;
         // Set the value
         int intValueHue = (int) hue;
-        LedController.ledBuffer.setHSV(i, intValueHue, 255, 128);
+        ledBuffer.setHSV(i, intValueHue, 255, 128);
       }
       // Increase by to make the rainbow "move"
       m_rainbowFirstPixelHue += 3;
