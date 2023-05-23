5558d31b8ef4de3b3f0bd504a12ee3503dd3802b
wheeleral <awheeler2@wpi.edu>
2023-05-23 18:22:25 -0400

    Anna's work on leds and fun stuff with that

--- competition/src/main/java/frc/robot/Led/Purple.java
+++ competition/src/main/java/frc/robot/Led/Purple.java
@@ -1,14 +1,34 @@
 package frc.robot.Led;
 
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Purple implements Led {
+    Timer timer = new Timer();
+
     public void start(AddressableLEDBuffer buffer, int length) {
+        timer.start();
     }
 
     public void run(AddressableLEDBuffer buffer, int length) {
         for (var i = 0; i < buffer.getLength(); i++) {
-            buffer.setRGB(i, 200,00, 200);
+            if(i%2 == 0){
+                if(timer.get() < 1){
+                    buffer.setRGB(i, 00, 75, 25);//200:0:200
+                } else{
+                    buffer.setRGB(i, 00,00, 00);
+                }
+            } else{
+                if(timer.get() > 1 && timer.get() < 2){
+                    buffer.setRGB(i, 128,64, 00);//200:0:200
+                } else{
+                    buffer.setRGB(i, 00,00, 00);
+                }
+            }
+
+            if(timer.get() > 2){
+                timer.restart();
+            }
         }
 //yellow 250, 90, 0 (but divide)
     }
