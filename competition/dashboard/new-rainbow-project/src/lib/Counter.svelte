8586acec0bc66a043b9a49a3a071f54bd715424d
tee-bee <teebeewiiu@gmail.com>
2023-11-06 20:44:29 -0500

    We were very safe today with environmentally friendly and healthy color changing pages in this set of files :D

---
+++ competition/dashboard/new-rainbow-project/src/lib/Counter.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 3030303;
+  }
+</script>
+
+<style>
+  button {
+    background: indigo;
+    font-size: 30px;
+  }
+</style>
+
+<button on:click={increment}>
+  count is {count}
+</button>
